c3c59bf0a570e47cd1f9b9704790dbd06e65a956
PRIYA-594 <97585592+PRIYA-594@users.noreply.github.com>
2023-07-05 20:40:55 +0530

    Add files via upload

---
+++ Program9a.java
@@ -0,0 +1,29 @@
+package PatternBasic;
+import java.util.*;
+public class Program9a {
+	public static void main(String args[])
+	{
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			for(int j =0;j<n;j++)
+			{
+				if(i+j<n)
+				{
+					System.out.print(" ");
+				}
+				else if(i+j==n)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
+		s.close();
+	}
+}
